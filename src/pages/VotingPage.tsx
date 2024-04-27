cc2e47e0b28b8ea3fc3e00f28e9e70d431b97b26
Kirill Sermyagin <sermyagin.abcen7@gmail.com>
2024-04-27 20:21:55 +0300

    VotingService

--- src/pages/VotingPage.tsx
+++ src/pages/VotingPage.tsx
@@ -1,5 +1,6 @@
 import { Button, Typography } from '@material-tailwind/react'
 import { useCallback, useEffect, useState } from 'react'
+import VotingService from '../API/VotingService'
 import RegionDescription from '../components/UI/RegionDescription/RegionDescription'
 import RegionsPicker from '../components/UI/RegionsPicker/RegionsPicker'
 import { getRegions } from '../utils/getRegions'
@@ -7,13 +8,25 @@ import { getRegions } from '../utils/getRegions'
 const VotingPage = () => {
 	const [regions] = useState(getRegions())
 	const [currentRegion, setCurrentRegion] = useState<string>('')
+	const [currentCountVotes, setCurrentCountVotes] = useState<number>(0)
 
-	const getCountVotes = useCallback(() => {
-		console.log(currentRegion)
-		return 12
+	const getCountVotes = useCallback(async () => {
+		setCurrentCountVotes(
+			(await VotingService.getCountVotes(currentRegion, 1)).votes
+		)
+	}, [currentRegion])
+
+	useEffect(() => {
+		getCountVotes()
 	}, [currentRegion])
 
 	const handleUpvote = useCallback(() => {
+		// TODO: Change to localstorage
+		VotingService.upvote(
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjYsImV4cCI6MTcxNDIzODgxMH0.m8vSxZJHZV80dY4MdfW3QsAD4vEqj6F6ZU_vrTbv38Y',
+			currentRegion,
+			1
+		)
 		console.log(currentRegion)
 	}, [currentRegion])
 
@@ -29,12 +42,15 @@ const VotingPage = () => {
 			<Typography className='ml-5' variant='h3' color='gray'>
 				Окружной этап
 			</Typography>
-			<RegionsPicker setExternalRegion={setCurrentRegion} className='m-5 max-w-[24em]'/>
+			<RegionsPicker
+				setExternalRegion={setCurrentRegion}
+				className='m-5 max-w-[24em]'
+			/>
 			{currentRegion && (
 				<div className='m-5'>
 					<RegionDescription
 						title={currentRegion}
-						countVotes={getCountVotes()}
+						countVotes={currentCountVotes}
 						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maxime dolores quae accusamus dolorum blanditiis, sunt velit, corporis molestias tenetur unde deleniti consequatur! Ad beatae qui et in doloremque quae.'
 					/>
 					<Button className='mt-5' onClick={handleUpvote}>
